820135690668da1a221667c985fc0b01f3888fa4
anupam <anupam@ucdconnect.ie>
2021-09-26 11:48:12 +0100

    new changes 1

--- src/test/java/suresh_rocks_pckg/surech_hero_rocks.java
+++ src/test/java/suresh_rocks_pckg/surech_hero_rocks.java
@@ -15,6 +15,7 @@ public class surech_hero_rocks {
 	
 	@Test
 	public void syso() throws MalformedURLException {
+		System.out.println("started----hello suresh rocks");
 		WebDriverManager.chromedriver().setup();
 		DesiredCapabilities dc=new DesiredCapabilities();
 		dc.setBrowserName("chrome");
